b4dfbe49e3bd49822d45422192cf8d4b9a69bd21
DerKleineLi <lhx393264301@163.com>
2022-08-15 00:54:43 +0200

    fix text gui, WIP: add buff-action seperation

--- easy_style_switch.lua
+++ easy_style_switch.lua
@@ -8,6 +8,7 @@ local cfg = json.load_file("easy_style_switch_settings.json")
 cfg = cfg or {}
 cfg.enabled = cfg.enabled or true
 cfg.disable_move_switch = cfg.disable_move_switch or true
+cfg.disable_buff_switch = cfg.disable_buff_switch or true
 cfg.keyboard_btn = cfg.keyboard_btn or 0
 cfg.keyboard_red = cfg.keyboard_red or 0
 cfg.keyboard_blue = cfg.keyboard_blue or 0
@@ -45,12 +46,6 @@ re.on_config_save(
 -- disable move switch
 local PlayerReplaceAtkMysetHolder = sdk.find_type_definition("snow.player.PlayerReplaceAtkMysetHolder");
 local changeReplaceAtkMyset = PlayerReplaceAtkMysetHolder:get_method("changeReplaceAtkMyset")
-local PlayerBase = sdk.find_type_definition("snow.player.PlayerBase");
-local reflectReplaceAtkMyset = PlayerBase:get_method("reflectReplaceAtkMyset");
-
-local setReplaceAtkMyset = PlayerBase:get_method("setReplaceAtkMyset");
-local setReplaceAtkType = PlayerBase:get_method("setReplaceAtkType");
-
 sdk.hook(changeReplaceAtkMyset,function(args)
     if cfg.disable_move_switch and cfg.enabled then
         return sdk.PreHookResult.SKIP_ORIGINAL
@@ -59,6 +54,8 @@ sdk.hook(changeReplaceAtkMyset,function(args)
     end
 end,function(retval) return retval; end)
 
+-- local PlayerBase = sdk.find_type_definition("snow.player.PlayerBase");
+-- local reflectReplaceAtkMyset = PlayerBase:get_method("reflectReplaceAtkMyset");
 -- sdk.hook(reflectReplaceAtkMyset,function(args)
 --     if cfg.disable_move_switch and cfg.enabled then
 --         return sdk.PreHookResult.SKIP_ORIGINAL
@@ -67,7 +64,138 @@ end,function(retval) return retval; end)
 --     end
 -- end,function(retval) return retval; end)
 
+-- hud hook
+local inject_hook = false;
+local SwitchActionSystem = sdk.find_type_definition("snow.data.SwitchActionSystem");
+local getEquippedActionMySetDataList = SwitchActionSystem:get_method("getEquippedActionMySetDataList(snow.data.weapon.WeaponTypes, snow.data.SwitchActionEquipType)");
+local getEquippedActionMySetDataList_args = nil;
+local script_myset_id = nil;
 
+local current_weapon = nil;
+local base_0 = nil;
+local base_1 = nil;
+local base_2 = nil;
+local base_3 = nil;
+local base_4 = nil;
+
+local stored_Myset_id = nil;
+local stored_0 = nil;
+local stored_1 = nil;
+local stored_2 = nil;
+local stored_3 = nil;
+local stored_4 = nil;
+
+sdk.hook(getEquippedActionMySetDataList,function(args) 
+    getEquippedActionMySetDataList_args = args;
+    return sdk.PreHookResult.CALL_ORIGINAL;
+end,function(retval) 
+    if not inject_hook then return retval end
+    -- log.debug("================")
+    -- log.debug("args:")
+    -- log.debug(sdk.to_int64(getEquippedActionMySetDataList_args[3]))
+    -- log.debug(sdk.to_int64(getEquippedActionMySetDataList_args[4]))
+
+    if stored_Myset_id ~= nil then
+        if stored_Myset_id ~= sdk.to_int64(getEquippedActionMySetDataList_args[4]) then return retval end
+        local mretval = sdk.to_managed_object(retval)
+        mretval[0] = stored_0;
+        mretval[1] = stored_1;
+        mretval[2] = stored_2;
+        mretval[3] = stored_3;
+        mretval[4] = stored_4;
+        local ptr = sdk.to_ptr(mretval)
+        if ptr ~= nil then
+            stored_Myset_id = nil;
+            return ptr
+        end
+        return retval;
+    end
+
+    if script_myset_id ~= 2 then
+        inject_hook = false;
+        return retval
+    end
+
+    if current_weapon ~= sdk.to_int64(getEquippedActionMySetDataList_args[3]) then
+        base_0 = nil;
+        base_1 = nil;
+        base_2 = nil;
+        base_3 = nil;
+        base_4 = nil;
+        current_weapon = sdk.to_int64(getEquippedActionMySetDataList_args[3])
+    end
+
+    local gui_manager = sdk.get_managed_singleton("snow.gui.GuiManager");
+    local guiHud_weaponTechniqueMyset = gui_manager:call("get_refGuiHud_WeaponTechniqueMySet");
+    local current_gui_set_id = guiHud_weaponTechniqueMyset:get_field("currentEquippedMySetIndex"):get_field("_Value")
+    if current_gui_set_id ~= sdk.to_int64(getEquippedActionMySetDataList_args[4]) then return retval end
+
+    stored_Myset_id = current_gui_set_id;
+
+    local mretval = sdk.to_managed_object(retval)
+    stored_0 = mretval[0];
+    stored_1 = mretval[1];
+    stored_2 = mretval[2];
+    stored_3 = mretval[3];
+    stored_4 = mretval[4];
+
+    -- log.debug("before")
+    -- log.debug(mretval[0])
+    -- log.debug(mretval[1])
+    -- log.debug(mretval[2])
+    -- log.debug(mretval[3])
+    -- log.debug(mretval[4])
+
+    if not base_0 then
+        local player_manager = sdk.get_managed_singleton("snow.player.PlayerManager");
+        local master_player = player_manager:call("findMasterPlayer");
+        if not master_player then return retval end
+        local player_replace_atk_myset_holder = master_player:get_field("_ReplaceAtkMysetHolder");
+        player_replace_atk_myset_holder:call("setSelectedMysetIndex", current_gui_set_id);
+
+        base_0 = mretval[0] - player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",0);
+        base_1 = base_0 + 2;
+        base_2 = mretval[2] - player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",3);
+        base_3 = base_1 + 2;
+        base_4 = base_2 + 3;
+        player_replace_atk_myset_holder:call("setSelectedMysetIndex", current_set_id);
+    end
+
+    mretval[0] = base_0 + cfg.switch_skill_1
+    mretval[1] = base_1 + cfg.switch_skill_2
+    mretval[2] = base_2 + cfg.switch_skill_3
+    if cfg.switch_skill_4 <= 1 then
+        mretval[3] = base_3 + cfg.switch_skill_4
+    elseif cfg.switch_skill_4 == 2 then
+        mretval[3] = base_2 + 2
+    end
+    mretval[4] = base_4 + cfg.switch_skill_5
+
+    -- log.debug("after")
+    -- log.debug(mretval[0])
+    -- log.debug(mretval[1])
+    -- log.debug(mretval[2])
+    -- log.debug(mretval[3])
+    -- log.debug(mretval[4])
+
+    -- log.debug("stored:")
+    -- log.debug(stored_0)
+    -- log.debug(stored_1)
+    -- log.debug(stored_2)
+    -- log.debug(stored_3)
+    -- log.debug(stored_4)
+
+    local ptr = sdk.to_ptr(mretval)
+    if ptr ~= nil then
+        inject_hook = false;
+        return ptr
+    end
+
+    return retval
+end)
+
+
+-- switch function
 local function switch_Myset(set_id)
     local player_manager = sdk.get_managed_singleton("snow.player.PlayerManager");
     local gui_manager = sdk.get_managed_singleton("snow.gui.GuiManager");
@@ -75,9 +203,9 @@ local function switch_Myset(set_id)
     -- log.debug(master_player:get_address())
     if not master_player then return end
     local player_replace_atk_myset_holder = master_player:get_field("_ReplaceAtkMysetHolder");
+    local current_set_id = player_replace_atk_myset_holder:call("getSelectedIndex");
 
     if not set_id then
-        local current_set_id = player_replace_atk_myset_holder:call("getSelectedIndex");
         if current_set_id == 0 then
             set_id = 1
         else
@@ -85,31 +213,42 @@ local function switch_Myset(set_id)
         end
     end
 
-    if set_id <= 1 then
-        -- switch Myset
-        player_replace_atk_myset_holder:call("setSelectedMysetIndex", set_id);
-        master_player:set_field("_replaceAttackTypeA", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",0))
-        master_player:set_field("_replaceAttackTypeB", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",1))
-        master_player:set_field("_replaceAttackTypeC", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",2))
-        master_player:set_field("_replaceAttackTypeD", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",3))
-        master_player:set_field("_replaceAttackTypeE", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",4))
-        master_player:set_field("_replaceAttackTypeF", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",5))
-        -- update hud
-        local guiHud_weaponTechniqueMyset = gui_manager:call("get_refGuiHud_WeaponTechniqueMySet");
-        guiHud_weaponTechniqueMyset:write_dword(0x118, set_id);
-    elseif set_id == 2 then
-        -- switch to third style
-        master_player:set_field("_replaceAttackTypeA", cfg.switch_skill_1)
-        master_player:set_field("_replaceAttackTypeB", cfg.switch_skill_2)
-        master_player:set_field("_replaceAttackTypeD", cfg.switch_skill_3)
-        master_player:set_field("_replaceAttackTypeF", cfg.switch_skill_5)
-        if cfg.switch_skill_4 <= 1 then
-            master_player:set_field("_replaceAttackTypeC", cfg.switch_skill_4)
-            master_player:set_field("_replaceAttackTypeE", 0)
-        elseif cfg.switch_skill_4 == 2 then
-            master_player:set_field("_replaceAttackTypeC", 0)
-            master_player:set_field("_replaceAttackTypeE", 1)
+    if script_myset_id ~= set_id then
+
+        if set_id <= 1 then
+            -- switch Myset
+            player_replace_atk_myset_holder:call("setSelectedMysetIndex", set_id);
+            master_player:set_field("_replaceAttackTypeA", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",0))
+            master_player:set_field("_replaceAttackTypeB", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",1))
+            master_player:set_field("_replaceAttackTypeC", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",2))
+            master_player:set_field("_replaceAttackTypeD", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",3))
+            master_player:set_field("_replaceAttackTypeE", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",4))
+            master_player:set_field("_replaceAttackTypeF", player_replace_atk_myset_holder:call("getReplaceAtkTypeFromMyset",5))
+            if cfg.disable_buff_switch then
+                player_replace_atk_myset_holder:call("setSelectedMysetIndex", current_set_id);
+            end
+            -- update hud
+            local guiHud_weaponTechniqueMyset = gui_manager:call("get_refGuiHud_WeaponTechniqueMySet");
+            -- guiHud_weaponTechniqueMyset:get_field("currentEquippedMySetIndex"):set_field("_Value", set_id);
+            guiHud_weaponTechniqueMyset:write_dword(0x118, set_id);
+        elseif set_id == 2 then
+            -- switch to third style
+            master_player:set_field("_replaceAttackTypeA", cfg.switch_skill_1)
+            master_player:set_field("_replaceAttackTypeB", cfg.switch_skill_2)
+            master_player:set_field("_replaceAttackTypeD", cfg.switch_skill_3)
+            master_player:set_field("_replaceAttackTypeF", cfg.switch_skill_5)
+            if cfg.switch_skill_4 <= 1 then
+                master_player:set_field("_replaceAttackTypeC", cfg.switch_skill_4)
+                master_player:set_field("_replaceAttackTypeE", 0)
+            elseif cfg.switch_skill_4 == 2 then
+                master_player:set_field("_replaceAttackTypeC", 0)
+                master_player:set_field("_replaceAttackTypeE", 1)
+            end
         end
+        script_myset_id = set_id;
+        -- update hud
+        inject_hook = true;
+
     end
 end
 
@@ -216,6 +355,9 @@ re.on_draw_ui(
         local changed, value = imgui.checkbox("Disable move switch", cfg.disable_move_switch)
         if changed then cfg.disable_move_switch = value end
 
+        local changed, value = imgui.checkbox("Disable buff switch", cfg.disable_buff_switch)
+        if changed then cfg.disable_buff_switch = value end
+
         if imgui.tree_node("Key bindings") then
 
             if imgui.button("Keyboard switch: " .. HwKeys.KeyboardKeys[cfg.keyboard_btn]) then
@@ -274,9 +416,35 @@ re.on_draw_ui(
         end
 
         if imgui.button("debug") then
+            -- snow.data.DataShortcut.getName(snow.data.DataDef.PlWeaponActionId)
+            -- snow.data.SwitchActionSystem.getEquippedActionMySetDataList(snow.data.weapon.WeaponTypes, snow.data.SwitchActionEquipType)
+            local system_data_manager = sdk.get_managed_singleton("snow.data.SystemDataManager");
+            local switch_action_system = system_data_manager:call("getSwitchActionSystem");
             local player_manager = sdk.get_managed_singleton("snow.player.PlayerManager");
+            local gui_manager = sdk.get_managed_singleton("snow.gui.GuiManager");
             local master_player = player_manager:call("findMasterPlayer");
-            local boolean = master_player:call("setReplaceAtkMyset")
+            local weapon_data = master_player:call("getWeaponListData");
+            local weapon_types = weapon_data:call("get_DtWeaponType");
+            local player_replace_atk_myset_holder = master_player:get_field("_ReplaceAtkMysetHolder");
+            local current_set_id = player_replace_atk_myset_holder:call("getSelectedIndex");
+            local gui_manager = sdk.get_managed_singleton("snow.gui.GuiManager");
+            local guiHud_weaponTechniqueMyset = gui_manager:call("get_refGuiHud_WeaponTechniqueMySet");
+            local current_gui_set_id = guiHud_weaponTechniqueMyset:get_field("currentEquippedMySetIndex"):get_field("_Value")
+            local player_weapon_type = master_player:get_field("_playerWeaponType");
+            local action_data_list = switch_action_system:call("getEquippedActionMySetDataList(snow.data.weapon.WeaponTypes, snow.data.SwitchActionEquipType)", weapon_types, current_gui_set_id);
+            -- local all_action_data_list = switch_action_system:call("get_ActiveActionDataList");
+            -- local default_action_data_list = switch_action_system:call("getActionDataListForPlayer", player_weapon_type);
+            -- local default_action_data_list = switch_action_system:call("getActionDataList(snow.player.PlayerWeaponType)", player_weapon_type);
+            -- for i=0,13 do
+            --     for j=0,4 do
+            --         log.debug("(" .. i .. "," .. j .. "): " .. all_action_data_list[i][j]:call("get_ActionId"))
+            --     end
+            -- end
+            log.debug(action_data_list:get_type_definition():get_full_name())
+            for i=0,4 do
+                log.debug(action_data_list[i]:get_field("value__"))
+            end
+
         end
 
         imgui.tree_pop()
